271f868c30b88b379b977d0e8c3832b2344957e6
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-02-23 12:47:55 +0000

    first servlet working, starting login form

---
+++ src/TestServlet.java
@@ -0,0 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns="/first")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //resp.getWriter().println("<p>Get request working </p>");
+
+        // pass value to jsp
+        req.setAttribute("name", "conor");
+
+        // redirect to jsp
+        req.getRequestDispatcher("/WEB-INF/views/first.jsp").forward(req, resp);
+    }
+
+}
---
+++ web/WEB-INF/views/first.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: conor
+  Date: 23/02/2017
+  Time: 12:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First Page</title>
+</head>
+<body>
+
+    <%--expression language ${}--%>
+    <p>Hello ${name}</p>
+
+</body>
+</html>
